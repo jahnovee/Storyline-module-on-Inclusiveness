81735fbd91895d6fd4831c0951ffde569a09e9a1
jahnovee <30384354+jahnovee@users.noreply.github.com>
2026-03-25 23:51:17 +0530

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,48 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6KjSMuvkL8R":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('66es0ohPwE3');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6IR4fGADZZD';
+const shrinkAmount = 0.2;
+const delay = 0;
+addToTimeline(
+target.animate(
+[ {scale: `${1 - shrinkAmount}` } ]
+,
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
